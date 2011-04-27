d06593c52417d115a17b2d7daf102ba568d622e5
U-Tearotea-PC\Tearotea <Tearotea@Tearotea-PC.(none)>
2011-04-27 21:25:30 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cb19a36feb1f283d020807483f1543b6499b96f5261a828bfde0233311582aa4db8c10fdd15d7678d159aeb47dd09d886ff3d2c65fc5b6ad5811155e7983ab61'
